774135e57f4162a60626b0b57bee9c1ffa9c2c2f
hitanshuk29 <131183929+hitanshuk29@users.noreply.github.com>
2025-05-09 16:43:25 +0530

    Update hooks.py

--- supernatural/supernatural/hooks.py
+++ supernatural/supernatural/hooks.py
@@ -44,7 +44,7 @@ app_license = "mit"
 
 # include js in doctype views
 doctype_js = {
-    "Lead": "supernatural/public/js/lead.js"
+    "Lead": "public/js/lead.js"
 }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
